121533109f28230ea4896fd96489b54e6c4630a9
Rodrigo Mantica <manticarodrigo@gmail.com>
2019-11-30 19:12:30 -0500

    Replaced how it works hero with global.

--- src/pages/HowItWorks/index.js
+++ src/pages/HowItWorks/index.js
@@ -1,18 +1,38 @@
 import React from 'react';
 
+import HeroBgImage from 'assets/jpgs/how-it-works-hero-bg.jpg';
+
+import Hero from 'components/Hero';
 import MocaSection from 'components/MocaSection';
 import MocaWaySection from 'components/MocaWaySection';
-import ReviewsSection from 'components/ReviewsSection';
+// import ReviewsSection from 'components/ReviewsSection';
 import FooterForm from 'components/FooterForm';
 
-import Hero from './Hero';
+// import Hero from './Hero';
+
+const heroTitle = (
+  <>
+    PHYSICAL THERAPY DELIVERED TO
+    {' '}
+    <span style={{ color: '#71cfeb' }}>YOU</span>
+    .
+  </>
+);
+
+const heroDescription = (
+  <>
+    MOCA was created to meet the needs of both patient and the therapist,
+    empowering them with time and capacity, ability to manage their own schedule
+    and aid in facilitating a high-quality medical service at a location most convenient to you.
+  </>
+);
 
 const HowItWorksScreen = () => (
   <>
-    <Hero />
+    <Hero bgImage={HeroBgImage} title={heroTitle} description={heroDescription} />
     <MocaSection />
     <MocaWaySection />
-    <ReviewsSection />
+    {/* <ReviewsSection /> */}
     <FooterForm />
   </>
 );
